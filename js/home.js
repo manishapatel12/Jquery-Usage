304429f06ba78b1f76f507aab04658c6fb0d0c13
mpatel <mpatel@shutterstock.com>
2016-03-18 23:02:51 -0400

    jquery sticky and jquery jR3DCarousel example usage

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(window).load(function(){
+      $("#header_navigation").sticky({ topSpacing: 0 });
+});
+
+$(window).load(function(){
+      $("#footer_navigation").sticky({ bottomSpacing: 0 });
+});
+
+$('.jR3DCarouselGallery').jR3DCarousel({
+                    slides: slides
+                });
